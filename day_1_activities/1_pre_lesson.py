c936693e51ecf72f73349976843c962b6f3066e9
bsalmeron1 <bsalmeron1426@gmail.com>
2025-12-15 20:08:53 +0000

    dictonaries

--- day_1_activities/1_pre_lesson.py
+++ day_1_activities/1_pre_lesson.py
@@ -1,1 +1,27 @@
 # this is what we will use for the video intro to dictionaries
+
+#dic= collection of {key:value} pairs prdered and changeable no dupes
+capitals={ "USA" : "Washington D.C",
+"India" : "New Dehli",
+"China" : "Beijing",
+"Russia" : "moscow"}
+
+print(capitals.get("USA"))
+
+if capitals.get("China") :
+    print ("that capital exists")
+else:
+        print("That capital doesnt exist")
+
+keys= capitals.keys()
+for key in capitals.keys():
+      print(key)
+#capitals.popitem
+#capitals.keys to get them without values
+values= capitals.values() #will return value object fro every
+for value in capitals.values():
+      print(value)
+
+#items method
+for key, value in capitals.items():
+      print(f"{key}:{value}")
